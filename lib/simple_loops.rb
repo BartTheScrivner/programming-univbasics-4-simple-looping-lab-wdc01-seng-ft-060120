7f7a73c123ea730a812b5de0e1fbd979622ea12d
fantastic-scope-1514 <dframe22@gmail.com>
2020-04-21 21:04:59 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -2,19 +2,33 @@
 def loop_message_five_times(string)
   count = 0
   while count < 5 do
-    puts message
+    puts string
     count += 1
   end
 end
 
 def loop_message_n_times(string, n)
-
+  count = 0
+  while count < n do
+    puts string
+    count += 1
+  end
 end
 
 def output_array(array)
-
+  count = 0
+  while count < array.length do
+    puts array[count]
+    count += 1
+  end
 end
 
 def return_string_array(array)
-
+  count = 0
+  new_array = []
+  while count < array.length do
+    new_array.push(array[count].to_s) # OR new_array << array[count].to_s
+    count += 1
+  end
+  new_array
 end
